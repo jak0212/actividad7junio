5a11730ac4dcc821bc71bbb5753cf4d777e873bc
jak0212 <127949319+jak0212@users.noreply.github.com>
2023-06-09 18:52:18 -0500

    ejemplo 2

--- vite-project/src/components/Message.jsx
+++ vite-project/src/components/Message.jsx
@@ -1,19 +1,27 @@
-import React, {useState} from 'react';
-import "./Message.css" 
+import React, { useState } from 'react';
+import "./Message.css";
 
-const Message = ({text})=> {
-    const [showMessage ,setShowMessage]=useState(false)
+const Message = ({ text }) => {
+  const [showMessage, setShowMessage] = useState(false);
 
-    const handleClick = () =>{
-        setShowMessage(!showMessage);
-    };
+  const handleClick = () => {
+    setShowMessage(!showMessage);
+  };
 
-    return (
-        <div>
-            <button onClick={handleClick}>Mostrar/Ocultar</button>
-            {showMessage && <p className='Message'> {text}</p>} </div>
-    );
-}
+  return (
+    <div>
+      <header>
+        <h1>Esto es un header</h1>
+      </header>
+      <main>
+        <button onClick={handleClick}>Mostrar/Ocultar</button>
+        {showMessage && <p className='Message'>{text}</p>}
+      </main>
+      <footer>
+        <p>Kevin Eduardo Hernandez Botero  Adso 07/06/23</p>
+      </footer>
+    </div>
+  );
+};
 
-export default  Message;
-
+export default Message;
